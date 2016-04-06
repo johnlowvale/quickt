3a4fee33ae831e550cfbd4a6479d562d39133dad
John Lowvale <johnlowvale@hotmail.com>
2016-04-06 15:48:24 +0700

    added get_cookie, set_cookie

--- dev/quickt.js
+++ dev/quickt.js
@@ -57,6 +57,32 @@ function log(Var) {
 } 
 
 /**
+ * Get a cookie
+ */
+function get_cookie(cname) {
+  var name = cname+"=";
+  var ca   = document.cookie.split(';');  
+  for (var i=0; i<ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') 
+      c = c.substring(1);
+    if (c.indexOf(name) == 0) 
+      return c.substring(name.length,c.length);
+  }
+  return "";
+}
+
+/**
+ * Set a cookie
+ */
+function set_cookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime()+(exdays*24*60*60*1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname+"="+cvalue+"; "+expires;
+}
+
+/**
  * HTTP GET       
  * Callback is function(Error,Data)
  */        
